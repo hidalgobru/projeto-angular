32c581ea19303d897db33ea059404b2a82711c73
Bruna Hidalgo <bruna.hidalgo123@gmail.com>
2025-09-22 15:56:53 -0300

    Criação do módulo 'songs'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+//arquivo de rotas global
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'songs' },
+  { path: 'songs', loadChildren:() => import('./songs/songs.module').then(m => m.SongsModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
