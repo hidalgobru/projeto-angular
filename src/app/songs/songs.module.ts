1b2b503ce7f0ed84c8dc7deb21750693382f27fe
Bruna Hidalgo <bruna.hidalgo123@gmail.com>
2025-09-25 03:59:07 -0300

    Aplicando toolbar da biblioteca de faixas

--- src/app/songs/songs.module.ts
+++ src/app/songs/songs.module.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { SongsRoutingModule } from './songs-routing.module';
 import { SongsComponent } from './songs/songs.component';
+
 
 @NgModule({
   declarations: [SongsComponent],
@@ -12,7 +15,9 @@ import { SongsComponent } from './songs/songs.component';
     CommonModule,
     SongsRoutingModule,
     MatTableModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatCardModule,
+    MatToolbarModule
   ]
 })
 export class SongsModule { }
